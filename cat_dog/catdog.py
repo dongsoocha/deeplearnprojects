a54ce3a98b4cfbcc1f28b6459915519b9e9cc7b9
Chubbibanana <dongsoo.tchah@gmail.com>
2021-05-11 20:06:45 -0700

    add callback

--- cat_dog/catdog.py
+++ cat_dog/catdog.py
@@ -58,3 +58,10 @@ model.compile(loss='categorical_crossentropy',
     optimizer='rmsprop',metrics=['accuracy'])
 
 model.summary()
+
+# learning rate + callbacks
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
+earlystop = EarlyStopping(patience=10)
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc',patience=2,
+    verbose=1,factor=0.5,min_lr=0.00001)
+callbacks=[earlystop, learning_rate_reduction]
